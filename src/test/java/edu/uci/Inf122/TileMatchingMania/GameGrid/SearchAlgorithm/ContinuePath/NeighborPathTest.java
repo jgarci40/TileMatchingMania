812de7de769daa44a777ac2b4aa54736e30305bb
ALMaclaine <almaclaine@gmail.com>
2020-03-05 19:32:36 -0800

    Restructure SearchAlgorithm

--- src/test/java/edu/uci/Inf122/TileMatchingMania/GameGrid/SearchAlgorithm/ContinuePath/NeighborPathTest.java
+++ src/test/java/edu/uci/Inf122/TileMatchingMania/GameGrid/SearchAlgorithm/ContinuePath/NeighborPathTest.java
@@ -1,7 +1,9 @@
-package edu.uci.Inf122.TileMatchingMania.GameGrid.SearchAlgorithm.CollectionConditions;
+package edu.uci.Inf122.TileMatchingMania.GameGrid.SearchAlgorithm.ContinuePath;
 
 import edu.uci.Inf122.TileMatchingMania.GameGrid.GameGrid;
+import edu.uci.Inf122.TileMatchingMania.GameGrid.SearchAlgorithm.ContinuePath.Paths.NeighborPath;
 import edu.uci.Inf122.TileMatchingMania.GameGrid.Tile;
+import edu.uci.Inf122.TileMatchingMania.GameGrid.ValidPaths;
 import edu.uci.Inf122.TileMatchingMania.State.State;
 import edu.uci.Inf122.TileMatchingMania.State.StateCollection;
 import edu.uci.Inf122.TileMatchingMania.State.TestState;
@@ -9,46 +11,47 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class NeighborConditionTest {
+class NeighborPathTest {
 
     @Test
     void testAll() throws Exception {
-        collectionCondition();
+        continuePaths();
     }
 
-    void collectionCondition() throws Exception {
-        NeighborCondition nc = new NeighborCondition();
+    void continuePaths() throws Exception {
+        NeighborPath np = new NeighborPath();
         int rows = 5;
         int cols = 5;
         State state = new State(){};
         State state2 = new TestState();
+
         StateCollection sc = new StateCollection();
         sc.setDefaultState(state);
         GameGrid gg = new GameGrid(rows, cols, sc);
         sc.addState(state2);
 
         Tile t1 = gg.getTile(1, 1);
-        boolean collect = nc.collectionCondition(t1);
-        assertTrue(collect);
+        ValidPaths vps = np.continuePaths(t1);
+        assertTrue(vps.getDown());
+        assertTrue(vps.getLeft());
+        assertTrue(vps.getRight());
+        assertTrue(vps.getUp());
 
         gg.setState(0, 1, state2);
         gg.setState(1, 0, state2);
         gg.setState(1, 2, state2);
         gg.setState(2, 1, state2);
-        collect = nc.collectionCondition(t1);
-        assertFalse(collect);
+        vps = np.continuePaths(t1);
+        assertFalse(vps.getDown());
+        assertFalse(vps.getLeft());
+        assertFalse(vps.getRight());
+        assertFalse(vps.getUp());
 
         gg.setState(2, 1, state);
-        collect = nc.collectionCondition(t1);
-        assertTrue(collect);
-
-        gg.setState(4, 3, state2);
-        Tile t2 = gg.getTile(4, 4);
-        collect = nc.collectionCondition(t2);
-        assertTrue(collect);
-
-        gg.setState(3, 4, state2);
-        collect = nc.collectionCondition(t2);
-        assertFalse(collect);
+        vps = np.continuePaths(t1);
+        assertTrue(vps.getDown());
+        assertFalse(vps.getLeft());
+        assertFalse(vps.getRight());
+        assertFalse(vps.getUp());
     }
 }
