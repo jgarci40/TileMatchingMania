e72d8e79b5d655b08c499bf2d1a5c08311fb593d
ALMaclaine <almaclaine@gmail.com>
2020-03-05 19:35:22 -0800

    Add Game class

---
+++ src/main/java/edu/uci/Inf122/TileMatchingMania/Game/Game.java
@@ -0,0 +1,36 @@
+package edu.uci.Inf122.TileMatchingMania.Game;
+
+import edu.uci.Inf122.TileMatchingMania.GUI.Input.Input;
+import edu.uci.Inf122.TileMatchingMania.GameGrid.GameGrid;
+import edu.uci.Inf122.TileMatchingMania.GameGrid.Tile;
+import edu.uci.Inf122.TileMatchingMania.State.StateCollection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Game {
+    GameGrid gameGrid;
+    int rows;
+    int cols;
+    Map<String, StateCollection> collections;
+    int score;
+
+    public Game(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.collections = new HashMap<>();
+        this.score = 0;
+    }
+
+    public abstract boolean initGame();
+
+    public int getScore() { return score; }
+
+    public void setScore(int score) { this.score = score; }
+
+    public Tile[][] getGrid() {
+        return gameGrid.getGrid();
+    }
+
+    public abstract void nextInput(Input input);
+}
