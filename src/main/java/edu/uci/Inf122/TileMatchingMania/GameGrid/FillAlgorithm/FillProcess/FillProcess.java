3b6307b7ba87e9e4111bc63dfcdb4ff959585f06
Swan Toma <sktoma@uci.edu>
2020-03-11 23:59:56 -0700

    Commented FillProcess.java

--- src/main/java/edu/uci/Inf122/TileMatchingMania/GameGrid/FillAlgorithm/FillProcess/FillProcess.java
+++ src/main/java/edu/uci/Inf122/TileMatchingMania/GameGrid/FillAlgorithm/FillProcess/FillProcess.java
@@ -4,8 +4,26 @@ import edu.uci.Inf122.TileMatchingMania.GameGrid.FillAlgorithm.FillGenerator.Fil
 import edu.uci.Inf122.TileMatchingMania.GameGrid.Tile;
 import edu.uci.Inf122.TileMatchingMania.State.StateCollection;
 
+/*
+ * A fill process will populate tiles in a grid in some specified manner 
+ * depending on what fill algorithm is selected. For example, a gird can 
+ * be filled linearly from top to bottom if a linear fill is used.
+ */
 public abstract class FillProcess {
+	/*
+	 * To completely fill a grid based where tile States are determiend by the
+	 * fill generator provided.
+	 * 
+	 *  @param tileGrid The 2D grid that contains all the Tiles.
+	 *  @param fg A generator that will support setting each tile State.
+	 */
     public abstract void fillProcess(Tile[][] tileGrid, FillGenerator fg) throws Exception;
 
+    /*
+     * Set a tile's State object.
+     * 
+     * @param tile A specific tile that comes from the grid.
+	 *  @param fg A generator that will support setting each tile State.
+     */
     protected abstract void fillTile(Tile tile, FillGenerator fg) throws Exception;
 }
