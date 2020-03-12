5acf7a7642c72b19e2cfac227aa5fd590c739ccf
Swan Toma <sktoma@uci.edu>
2020-03-11 18:56:36 -0700

    Commented RandomFillAlgorithm.java

--- src/main/java/edu/uci/Inf122/TileMatchingMania/GameGrid/FillAlgorithm/Algorithms/RandomFillAlgorithm.java
+++ src/main/java/edu/uci/Inf122/TileMatchingMania/GameGrid/FillAlgorithm/Algorithms/RandomFillAlgorithm.java
@@ -6,7 +6,17 @@ import edu.uci.Inf122.TileMatchingMania.GameGrid.FillAlgorithm.FillGenerator.Gen
 import edu.uci.Inf122.TileMatchingMania.GameGrid.FillAlgorithm.FillProcess.Processes.LinearFillProcess;
 import edu.uci.Inf122.TileMatchingMania.State.StateCollection;
 
+/*
+ * A random fill algorithm linearly fills a grid from top to bottom and 
+ * each tile will take on a random State among the random States stored
+ * in FillAlgorithm.
+ */
 public class RandomFillAlgorithm extends FillAlgorithm {
+	/*
+	 * DefaultFillAlgorithm constructor.
+	 * 
+	 * @param stateCollection Hold State objects used in the game.
+	 */
     public RandomFillAlgorithm(StateCollection stateCollection) {
         super(new LinearFillProcess(), new RandomGenerator(stateCollection));
     }
