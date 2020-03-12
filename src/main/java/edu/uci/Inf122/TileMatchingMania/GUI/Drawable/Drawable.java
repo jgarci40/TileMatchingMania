7570c2d065bd25025a08521ff9a7e57b230bffac
Swan Toma <sktoma@uci.edu>
2020-03-12 15:37:52 -0700

    Commented Drawable.java, TextSquare.java, CoordinateInput.java

--- src/main/java/edu/uci/Inf122/TileMatchingMania/GUI/Drawable/Drawable.java
+++ src/main/java/edu/uci/Inf122/TileMatchingMania/GUI/Drawable/Drawable.java
@@ -2,6 +2,10 @@ package edu.uci.Inf122.TileMatchingMania.GUI.Drawable;
 
 import java.awt.*;
 
+/*
+ * Each Drawable type will have to implement a draw method
+ * so that a tile's visual representation can be rendered in a GUI.
+ */
 public interface Drawable {
     Image draw();
 }
